8ef3c4b3e35c779ec7423c2cfb06b7dd28f9a9ce
Jakub Basinski <jbgopro@gmail.com>
2022-12-07 12:14:39 +0100

    shop

---
+++ app.js
@@ -0,0 +1,133 @@
+require('dotenv').config();
+
+const express = require('express');
+const session = require('express-session');
+
+const mongoose = require('mongoose');
+const MongoDBStore = require('connect-mongodb-session')(session);
+
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const helmet = require('helmet');
+const csrf = require('csurf');
+const flash = require('connect-flash');
+const multer = require('multer');
+const compression = require('compression');
+const errorController = require('./controllers/error');
+const User = require('./models/user');
+const app = express();
+const dotenv = require('dotenv')
+
+const MONGO_URI = process.env.MONGO_COMPASS
+dotenv.config()
+
+const store = new MongoDBStore({
+  uri: MONGO_URI,
+  collection: 'sessions',
+});
+
+const csrfProtection = csrf();
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().getMilliseconds().toString() + '-' + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
+app.use(csrfProtection);
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then((user) => {
+      if (!user) {
+        return next();
+      }
+      req.user = user;
+      next();
+    })
+    .catch((err) => {
+      next(new Error(err));
+    });
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+app.use(helmet());
+app.use(compression());
+
+app.get('/500', errorController.get500);
+
+app.use(errorController.get404);
+
+app.use((error, req, res, next) => {
+  res.status(500).render('500', {
+    pageTitle: 'Error!',
+    path: '/500',
+    isAuthenticated: req.session.isLoggedIn,
+  });
+});
+
+mongoose.connect(
+  MONGO_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) {
+      console.error('FAILED TO CONNECT TO MONGODB');
+      console.error(err);
+    } else {
+      console.log('CONNECTED TO MONGODB');
+      app.listen(3013);
+    }
+  }
+);
